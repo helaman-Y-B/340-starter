877e92471cf629165477c40ca241f176f354f308
Helamã Y. Barbour <helamashi@gmail.com>
2025-03-07 09:08:17 -0300

    added comments for the javascript

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,19 @@
+// Script for the dropdown menu on the mobile version of the website
+
 const currentPage = document.getElementById("current");
 const dropdown = document.getElementById("dropdown");
 const nav = document.getElementsByTagName("nav")[0];
 
+// Function to update the dropdown menu based on the screen width
 function updateDropdown() {
   const screenWidth = window.innerWidth;
+
+  // If the screen width is less than 565px, the dropdown menu will be displayed as a block
+  // Otherwise, the dropdown menu will be displayed as a flex container
   if (screenWidth < 565) {
+
+    // If the dropdown menu is already displayed as a block, the position and background color will be changed
+    // Otherwise, the dropdown menu will be displayed as a block and the background color will be changed
     if (dropdown.style.display === "block") {
       dropdown.style.position = "relative";
       dropdown.style.backgroundColor = "rgb(0, 162, 255)";
@@ -23,7 +32,11 @@ function updateDropdown() {
     nav.style.backgroundColor = "rgb(140, 213, 255)";
   }
 
+  // Event listener for the current page button
   currentPage.addEventListener("click", () => {
+
+    // If the dropdown menu is displayed as a block, it will be hidden and the background color will change
+    // Otherwise, the dropdown menu will be displayed as a block and the background color will be updated
     if (dropdown.style.display === "block") {
       dropdown.style.display = "none";
       nav.style.backgroundColor = "rgb(140, 213, 255)";
@@ -34,6 +47,7 @@ function updateDropdown() {
   });
 }
 
+// Event listener for the window resize event
 window.addEventListener("resize", updateDropdown);
 
 // Initial call to set the correct state on page load
