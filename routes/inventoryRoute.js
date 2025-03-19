20624680566971a5a63d2049fbc469f661e46bb5
Helamã Y. Barbour <helamashi@gmail.com>
2025-03-19 09:52:00 -0300

    Error handling is almost done, same thing with the inventory views.
    Just need to add some CSS now for the web page.

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,9 +1,9 @@
 // Needed Resources
 const express = require("express");
 const router = new express.Router();
-const invController = require("../controllers/inventoryController");
+const invController = require("../controllers/invController");
 
 // Route to build inventory by classification view
-router.get("/type/:classficationId", invController.buildByClassificationId);
+router.get("/type/:classificationId", invController.buildByClassificationId);
 
 module.exports = router;
