0d68c3f3a42bf5826bfb11c4f1bc020c5ff64d28
helaman-Y-B <90625373+helaman-Y-B@users.noreply.github.com>
2025-03-27 00:51:14 +0000

    Made so that the showpassword.js file also changes the text in the show password button

--- public/js/showPassword.js
+++ public/js/showPassword.js
@@ -5,7 +5,9 @@ const showPassword = document.getElementById("show-password");
 showPassword.addEventListener("click", () => {
 if (passwordField.type === "password") {
   passwordField.type = "text";
+  showPassword.textContent = "Hide Password";
 } else {
   passwordField.type = "password";
+    showPassword.textContent = "Show Password";
 }
 });
