dd78ea1e00a15a7e2461ff7f3a4de487fee1020f
helaman-Y-B <90625373+helaman-Y-B@users.noreply.github.com>
2025-04-11 00:50:26 +0000

    Basically finished the buy vehicle. Created a new table and made one to many conecctions to with the account table and inventory table.
    Also made the orders list page work but need to make the css for it.

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,12 +1,15 @@
 const utilities = require("../utilities/index");
 const invModel = require("../models/inventory-model");
-//const orderModel = require("../models/order-model");
+const orderModel = require("../models/order-model");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 require("dotenv").config()
 
 const orderController = {}
 
+/* ***************************
+ *  Build buy form
+ * ************************** */
 orderController.buildBuyForm = async function (req, res) {
     const inv_id = req.params.inv_id;
     const nav = await utilities.getNav();
@@ -17,11 +20,45 @@ orderController.buildBuyForm = async function (req, res) {
         title: "Buy Form",
         nav,
         loggedIn: res.locals.loggedIn,
+        inv_id,
         accountData,
         vehicleGrid,
         errors: null,
         message: req.flash("message"),
     })
+}
+
+/* ***************************
+ *  Build order list
+ * ************************** */
+orderController.buildOrderList = async function (req, res) {
+    const nav = await utilities.getNav();
+    const accountData = res.locals.accountData;
+    const orders = await orderModel.getAllOrders();
+    res.render("orders/orders-list", {
+        title: "Orders List",
+        nav,
+        loggedIn: res.locals.loggedIn,
+        accountData,
+        orders,
+        errors: null,
+    })
+}
+
+orderController.sendOrder = async function (req, res) {
+    const inv_id = req.params.inv_id;
+    //const nav = await utilities.getNav();
+    //const accountData = res.locals.accountData;
+    const date = new Date();
+    const order = req.body;
+    const data = await orderModel.addOrder(order, inv_id, date);
+    if (data) {
+        req.flash("message", "Order has been Placed Successfully");
+        res.redirect("/");
+    } else {
+        req.flash("message", "Error placing order");
+        res.redirect("/orders/buy-form/" + inv_id);
+    }
 }
 
 module.exports = orderController;
